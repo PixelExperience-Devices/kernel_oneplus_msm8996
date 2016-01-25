90f317bd14ea3f3e423063cbe1bd56368628c5d6
Wolfram Sang <wsa+renesas@sang-engineering.com>
2016-01-25 20:18:12 +0100

    mmc: make MAN_BKOPS_EN message a debug
    commit 4ec96b4cbde8d5714a4477b5a2562c3dd40bc5fa upstream.
    
    IMO this info is only useful for developers. Most users won't need this
    information, since there is not much they can do about it.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I8a9a0eefb0158b4e077fe9485faa89c497fe190f
    Signed-off-by: Lee Jones <joneslee@google.com>
    
     Conflicts:
    	drivers/mmc/core/mmc.c
    
    Change-Id: I6fe483f4bf811cc167af4c47c66efba313cd5207

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -587,10 +587,9 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 			card->ext_csd.bkops_en = ext_csd[EXT_CSD_BKOPS_EN];
 			card->ext_csd.raw_bkops_status =
 				ext_csd[EXT_CSD_BKOPS_STATUS];
-			pr_info("%s: BKOPS_EN equals 0x%x\n",
+			pr_debug("%s: BKOPS_EN equals 0x%x\n",
 					mmc_hostname(card->host),
 					card->ext_csd.bkops_en);
-
 		}
 
 		card->ext_csd.rel_param = ext_csd[EXT_CSD_WR_REL_PARAM];
