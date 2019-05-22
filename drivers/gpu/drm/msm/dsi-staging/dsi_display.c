d7e861611c8e7634d2bdfc855624b2d7b679f4aa
Nirmal Abraham <nabrah@codeaurora.org>
2019-05-22 10:09:34 +0530

    drm/msm/dsi-staging: Add range check in debugfs_dump_info_read
    Add a check to make sure that the length of bytes copied
    to the destination buffer doesn't exceed the requested
    buffer length before calling the copy_to_user to avoid
    buffer overflow.
    
    Change-Id: Icd65b9be2791a8a487dfc8d7461aadce61de3f1b
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -80,6 +80,9 @@ static ssize_t debugfs_dump_info_read(struct file *file,
 			"\tClock master = %s\n",
 			display->ctrl[display->clk_master_idx].ctrl->name);
 
+	if (len > user_len)
+		len = user_len;
+
 	if (copy_to_user(buff, buf, len)) {
 		kfree(buf);
 		return -EFAULT;
