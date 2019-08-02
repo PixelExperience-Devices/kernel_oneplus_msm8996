d6b4dcf63fa2b410ef2ac4574fee1acf14e19105
Charles Keepax <ckeepax@opensource.cirrus.com>
2019-08-02 09:26:27 -0400

    ALSA: compress: Prevent bypasses of set_params
    [ Upstream commit 26c3f1542f5064310ad26794c09321780d00c57d ]
    
    Currently, whilst in SNDRV_PCM_STATE_OPEN it is possible to call
    snd_compr_stop, snd_compr_drain and snd_compr_partial_drain, which
    allow a transition to SNDRV_PCM_STATE_SETUP. The stream should
    only be able to move to the setup state once it has received a
    SNDRV_COMPRESS_SET_PARAMS ioctl. Fix this issue by not allowing
    those ioctls whilst in the open state.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I6d5891bc58c4b1e66a708dddafabf20845c2a80a
    
     Conflicts:
    	sound/core/compress_offload.c

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -685,9 +685,15 @@ static int snd_compr_stop(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
-			stream->runtime->state == SNDRV_PCM_STATE_SETUP)
+	switch (stream->runtime->state) {
+	case SNDRV_PCM_STATE_OPEN:
+	case SNDRV_PCM_STATE_SETUP:
+	case SNDRV_PCM_STATE_PREPARED:
 		return -EPERM;
+	default:
+		break;
+	}
+
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_STOP);
 	if (!retval) {
 		stream->runtime->state = SNDRV_PCM_STATE_SETUP;
@@ -708,6 +714,7 @@ static int snd_compr_drain(struct snd_compr_stream *stream)
 
 	mutex_lock(&stream->device->lock);
 	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
+			stream->runtime->state == SNDRV_PCM_STATE_OPEN ||
 			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
 		retval = -EPERM;
 		goto ret;
@@ -753,6 +760,7 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 
 	mutex_lock(&stream->device->lock);
 	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
+			stream->runtime->state == SNDRV_PCM_STATE_OPEN ||
 			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
 		mutex_unlock(&stream->device->lock);
 		return -EPERM;
